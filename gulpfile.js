8ab3bce92df198ec5386d89026f12f818a38aee1
Luis Enrique <lepa930322@gmail.com>
2016-02-11 19:23:39 -0500

    Concat js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 var embedTemplates = require('gulp-angular-embed-templates');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('build', function () {
-    gulp.src('src/ld-table.js')
+    var jsFiles = [
+        'src/ldTable.module.js',
+        'src/ldTable.directive.js'
+    ];
+    gulp.src(jsFiles)
+        .pipe(concat('ldTable.js'))
         .pipe(embedTemplates())
         .pipe(gulp.dest('./dist'))
         .pipe(rename({ suffix: '.min' }))
